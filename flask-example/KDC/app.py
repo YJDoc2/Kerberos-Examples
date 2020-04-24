88637f2009bca717fd83159336f2f04091312866
Yashodhan Joshi <yash.sumedha@gmail.com>
2020-04-24 18:12:19 +0530

    Added comments to Flask-Example

--- flask-example/KDC/app.py
+++ flask-example/KDC/app.py
@@ -4,10 +4,22 @@ from flask import Flask, render_template, redirect, Response, jsonify,request
 from Kerberos import Kerberos_KDC,ServerError
 app = Flask(__name__, static_folder='./static', static_url_path='/')
 
+#* We create a KDC with ability to check and verify random
 kdc = Kerberos_KDC(check_rand=True)
+
+#? This part would idaelly be exposed via an admin only route, that would allow additino of servers to the network
+#? But here, We just add them with following code. Every time these are run a new ticket will be generated for Servers A and B
+#? Which must be copied EXACTLY same on the respective server's DB
+#? In case one wants to try that out, uncomment following two lines, run this script only,
+#? copy those generated tickets in Tickets folder in data-server folder,
+#? and them comment these line again and run the whole thing from start.
+
 #kdc.add_server('A')
 #kdc.add_server('B')
 
+#* This is our mock DB. In actual application this will be a connection to MongoDB or MySQLDb or some other DB
+#* but for this we just usea dictionary mapping username to plain passwords
+#! Note that the passwords should never be stored in plain text format in real application.
 users = {'u1':'pass1','u2':'pass2'}
 
 @app.route('/')
@@ -17,32 +29,64 @@ def homepage():
 @app.route('/api/login', methods=['POST'])
 def api_login():
     data = request.get_json()
+
+    #? Sanity checks
     if 'username' not in data or data['username'].strip() == '' or 'rand' not in data:
         return Response(json.dumps({'success': False, 'err': 'Incomplete Fields'}), mimetype="application/json", status=400)
 
+    #* Here we retrive the password from out 'DB'
+    #* in real application here one would contact a DB and query for username
     password = users.get(data['username'],None)
 
-    if password == None:
+
+    if password == None: #* No user Found
         return Response(json.dumps({'success': False, 'err': 'No User Found'}), mimetype="application/json", status=400)
+
+    #? Following is a possible way to convert a password to a secrete key.
+    #? Note that this process must be exactly repeated on client side, so that an exact same secrete key is 
+    #? generated on both sides, and response can be correctly decrypted. 
+    #! Final generated hash key must be of length 512 bits, i.e. 32 bytes total
+    #! NOTE Encoding or decoding from encodings like utf-8 or base 64 , 32 characters may not be total 256 bits 
     h = SHA256.new()
     h.update(password.encode('ascii'))
     hash_key = h.hexdigest()[0:32]
+
     try:
+        #* We get authentication and Ticket Granting Ticket from KDC
+        #* the auth ticket is encrypted with user's unique pass hash,
+        #* and it contains key to encrypt the request sent to TGS with
+        #? Here username and ip address is used as two uids for the function
         auth,tgt = kdc.gen_auth_tickets(data['rand'],data['username'],request.remote_addr,hash_key)
         return Response(json.dumps({'success': True, 'auth':auth.decode('ascii'),'tgt':tgt.decode('ascii')}), mimetype="application/json", status=200)
     except ServerError as e:
+        #! In case some error is occures which is Server error that is an error in generating the tickets, we send error response.
+        #! In case some other type of error is thrown, it may be due to incorrect configuration
+        #! Such as passing empty c_uid1 or c_uid2 fields or the hash_key is of incorrect type
        return Response(json.dumps({'success': False,'err':str(e)}), mimetype="application/json", status=400)
 
 @app.route('/tickets',methods=['POST'])
 def tickets():
     data = request.get_json()
+
+    #? Sanity checks
     if 'req' not in data:
         return Response(json.dumps({'success': False,'err':'No req found'}), mimetype="application/json", status=400)
     if 'tgt' not in data:
         return Response(json.dumps({'success': False,'err':'No tgt found'}), mimetype="application/json", status=400)
     try:
+        #? We first decode the req field that is sent inside the http request
         req = kdc.decrypt_req(data['req'],'u1',request.remote_addr,data['tgt'])
+        #TODO : Remove Parse json from decode req and separate decryption and rand check etc
+        #? then we verify that the random number sent in the req is used for the first time by the user to
+        #? prevent replay attacks. This is kept a distinct step from decoding the request,
+        #? as it is not necessary that the request be an json as encoded string
+        #? thus we can retrieve random number from it as it is stored and then verify it in this step.
         kdc.verify_rand('u1',request.remote_addr,req['rand'])
+
+        #? here we get response for the client and ticket that the client requested.
+        #? NOTE that the response is not a http response, but an encrypted response which only the client can open with 
+        #? the key sent in the auth ticket originally, and contains key to encrypt the requests
+        #? that are to be sent to the requested server.
         res,ticket = kdc.get_res_and_ticket('u1',request.remote_addr,data['tgt'],req['target'],req['rand'])
         return Response(json.dumps({'success': True,'res':res.decode('ascii'),'ticket':ticket.decode('ascii')}), mimetype="application/json", status=200)
     except ServerError as e:
--- flask-example/KDC/static/script.js
+++ flask-example/KDC/static/script.js
@@ -1,6 +1,12 @@
+//* We need client as global variable so we can access it from all functions
 let client = null;
+//? It may be possible that by the time our request sent to a server using Kerberos reches it,
+//? the ticket that we sent is gets expired , in edge cases.
+//? So we use a error delta here so than in refresh ticket function below if the remaining time for ticket's expiry is
+//? less than this we first take a fresh ticket and then make the request.
 const errDelta = 5000;
 
+//* Helper function to show errors in  error div
 function showError(e) {
   console.log(e);
   const err = document.querySelector('#error');
@@ -8,13 +14,22 @@ function showError(e) {
   err.innerHTML = errStr;
 }
 
+//* Helper function to clear the error div
 function clearError() {
   document.querySelector('#error').innerHTML = '';
 }
 
+//* A helper function to check if we have the required ticket,
+//* and if it is expired/close to expiry , take a new one
+//? name is the name of server we want the ticket for
+//? the ricket is saved in client by the 'name' and
+//? the response that is sent along with the ticket is decrypted and seved by name 'dec{name}'
+//? so for a server A, the ticket will be saved with name 'A' and decrypted response saved with name 'decA'
 async function refreshTicket(name) {
+  //* First check if ticket and decrypted response is present, returns undefined if it is not present
   const ticket = client.getTicket(name);
   const decTicket = client.getTicket(`dec${name}`);
+  //? Either is the ticket is not present or it is close to expiry, get a new ticket
   if (
     !ticket ||
     decTicket.timestamp + decTicket.lifetime < Date.now() + errDelta
@@ -26,6 +41,8 @@ async function refreshTicket(name) {
     req.uid2 = auth.uid2;
     req.rand = Math.floor(Math.random() * 1000);
     req.target = name;
+
+    //* Encrypt the request(data, not http) to be sent
     const encReq = client.encryptReq(req, auth.key);
 
     const response = (
@@ -34,23 +51,31 @@ async function refreshTicket(name) {
         tgt: client.getTicket('tgt'),
       })
     ).data;
+    //* Decrypt the response(data not http) sent with ticket
     const res = client.decryptRes(response.res, auth.key);
+    //* save decrypted response and ticket
     client.saveTicket(name, response.ticket);
     client.saveTicket(`dec${name}`, res);
   }
+  //* Either the ticket was already present or it is now brought from TGS
   return;
 }
 
+//* Initial Login Function
 async function login() {
+  //* Get data from the html elements
   let username = document.querySelector('#username').value;
   let pass = document.querySelector('#password').value;
+  //* clear the error div
   clearError();
 
   try {
+    //? Make a new random number to send along with request.
     const rand = Math.floor(Math.random() * 1000);
     const res = (
       await axios.post('/api/login', { username: username, rand: rand })
     ).data;
+    //* Here we make SHA 256 digest of password, exactly as it is done on server , and is used to encrypt the response
     const enc = new TextEncoder();
     const passEnc = enc.encode(pass);
     const hashBuffer = await crypto.subtle.digest('SHA-256', passEnc); // hash the message
@@ -59,40 +84,55 @@ async function login() {
       .map((b) => b.toString(16).padStart(2, '0'))
       .join(''); // convert bytes to hex string
     const key = hashHex.substr(0, 32);
+    //* Create the Client instance with the key
     client = new Kerberos.Client(key);
+    //* Try to decrypt response(data, not http) sent by server
+    //* Throws a error if incorrecr key, that is incorrect password
     const auth = client.decryptRes(res.auth, key);
+    //* If correct password save decrypted response as decAuth, encrypted response and Ticket Granting Ticket
     client.saveTicket('decAuth', auth);
     client.saveTicket('Auth', res.auth);
     client.saveTicket('tgt', res.tgt);
   } catch (e) {
     if (e instanceof ReferenceError) {
       showError('Incorrect Password');
+    } else {
+      throw e;
     }
   }
 }
 
+//* Function to get data from server A
 async function serverAGet() {
+  //* Clear ther error div
   clearError();
+  //? Sanity check, dont try if user has not successfully logged in
   if (!client) {
     showError('Please Log in with valid credentials');
     return;
   }
 
   try {
+    //* Make sure we have a working ticket for server A
     await refreshTicket('A');
     let req = {};
+    //* Make new random for sending
     req.rand = Math.floor(Math.random() * 1000);
 
     const ticket = client.getTicket('A');
     const decA = client.getTicket('decA');
+    //* Encrypt our request (data, not http) using the key given in response with ticket A
     const serEncReq = client.encryptReq(req, decA.key, decA.init_val);
+    //* send actual(http) request
     const response = (
       await axios.post('http://localhost:5001/data', {
         req: serEncReq,
         ticket: ticket,
       })
     ).data;
+    //* decrypt response by same key
     const res = client.decryptRes(response, decA.key, decA.init_val);
+    //* Show data on page
     let template = '';
     res.res.forEach((entry, i) => {
       template += `<h6>Book ${i + 1} : ${entry}</h6>`;
@@ -103,24 +143,37 @@ async function serverAGet() {
   }
 }
 
+//* Function to save data on server B
 async function serverASave() {
+  //* ckear the error div
   clearError();
+  //? Sanity check, dont try if user has not successfully logged in
   if (!client) {
     showError('Please Log in with valid credentials');
     return;
   }
+  //* Get the data that is to be sent to server from html element
   const input = document.querySelector('#dataA').value.trim();
+
+  //? sanity check if input is empty dont do anything
   if (input === '') {
     return;
   }
 
   try {
+    //* Make sure we have a working ticket for server A
     await refreshTicket('A');
+    //* make our request object as required by our server (the structure is not part of kerberos)
     let req = { book: input };
+    //* Make a random number to send along with request
     req.rand = Math.floor(Math.random() * 1000);
+    //* get the tickets
     const ticket = client.getTicket('A');
     const decA = client.getTicket('decA');
+    //* Encrypt the request with key sent in response along with the ticket
     const serEncReq = client.encryptReq(req, decA.key, decA.init_val);
+    //* make actual request
+    //? as our server does not send any data in response we don't try to decode it.
     const response = (
       await axios.post('http://localhost:5001/add', {
         req: serEncReq,
@@ -132,6 +185,7 @@ async function serverASave() {
   }
 }
 
+//! Same Login as ServerAGet()
 async function serverBGet() {
   clearError();
   if (!client) {
@@ -163,6 +217,7 @@ async function serverBGet() {
   }
 }
 
+//! Same Login as ServerASave()
 async function serverBSave() {
   clearError();
   if (!client) {
